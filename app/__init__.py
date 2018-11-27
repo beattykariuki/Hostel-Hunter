0691effaaad2ce8471c00c4c6c13180a91f986d6
StacyChebet <staceychebet23@gmail.com>
2018-11-27 17:01:30 +0300

    Initialize bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -10,3 +10,6 @@ def create_app(config_name):
     #Creating app configurations
     app.config.from_object(config_options[config_name])
 
+    #Initializing flask extensions
+    bootstrap.init_app(app)
+    
