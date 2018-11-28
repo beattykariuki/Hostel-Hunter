2bde4ee052a69a0f48e27777dc9b607140a3d619
beattykariuki <bettykariuki026@gmail.com>
2018-11-28 19:32:54 +0300

    files added

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,20 @@
 from flask import Flask 
 from flask_bootstrap import Bootstrap 
 from config import config_options
+from flask_login import LoginManager
 
-bootstrap = Bootstrap
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+bootstrap = Bootstrap()
+
 
 def create_app(config_name):
+
     app = Flask(__name__)
+    from .auth import as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
     #Creating app configurations
     app.config.from_object(config_options[config_name])
@@ -13,3 +22,7 @@ def create_app(config_name):
     #Initializing flask extensions
     bootstrap.init_app(app)
     
+    #Initializing Flask Extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
---
+++ app/models.py
@@ -0,0 +1,45 @@
+from . import db 
+from werkzeug.security import generate_password_hash,check_password_hash
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+    
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.Foreinkey('roles.id'))
+    password_hash = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}' 
+
+pass_secure = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self,password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_pssword(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy = "dynamic")
+
+    def __repr__(self):
+        return f'User {self.name}' 
+    
