ee38fe52c5943bbc939230de2b020b8aa0f5613a
GeGe-K <gloriagivondo@gmail.com>
2018-11-29 10:26:32 +0300

    added migrations

--- app/models.py
+++ app/models.py
@@ -1,15 +1,14 @@
 from . import db
 from datetime import datetime
 
-
 class Review(db.Model):
 
     __tablename__ = 'reviews'
 
     id = db.Column(db.Integer,primary_key = True)
-    review_id = db.Column(db.Integer,db.ForeignKey('hostels.id'))
+    # hostel_id = db.Column(db.Integer,db.ForeignKey('hostels.id'))
     review = db.Column(db.String())
-    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    # user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
     posted = db.Column(db.DateTime,default=datetime.utcnow)
 
     
